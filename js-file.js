e6db8462a4365f5ee7f126704872c00d0ea94bf1
Nathan <127000341+Not-Tobi@users.noreply.github.com>
2023-06-27 20:42:24 -0400

    Add normal, easer, rainbow, and reset buttons

--- js-file.js
+++ js-file.js
@@ -1,54 +1,101 @@
 let mouse;
+let random;
 let colour = 'black';
 let gridSize = 500;
 let userInput = 16;
-let squareSize = gridSize / userInput ;
-let squareTotal = userInput  * userInput;
+let squareSize;
+let squareTotal;
+interactive();
 
 // Each individual squares
-for (let i = 0; i < squareTotal; i++) {  
-    let grid = document.querySelector(".grid");
-    let square = document.createElement('div');
-    square.classList.add('square')
-    grid.appendChild(square);
-
-    square.setAttribute('style', `height: ${squareSize}px;`, `width: ${squareSize}px;`)
+function interactive() {  
+    function calculate () {
+        squareSize = gridSize / userInput ;
+        squareTotal = userInput  * userInput;
+        return;
+    }
+    calculate();
     
-    // Using your mouse to change colours
-    square.addEventListener('click', () => {
-        square.style.backgroundColor = colour;
-    })
+    for (let i = 0; i < squareTotal; i++) {  
+        let grid = document.querySelector(".grid");
+        let square = document.createElement('div');
+        square.classList.add('square')
+        grid.appendChild(square);
 
-    square.addEventListener('mousedown', () => {
-        mouse = true;
-    })
+        square.setAttribute('style', `height: ${squareSize}px;`, `width: ${squareSize}px;`)
+        
+        // Using your mouse to change colours
+        square.addEventListener('click', () => {
+            if (random) {
+                randomColour();
+                square.style.backgroundColor = colour;
+            }
+            else {
+                square.style.backgroundColor = colour;
+            }
+        })
 
-    square.addEventListener('mousemove', () => {
-        if (mouse) {
-        square.style.backgroundColor = colour;
-        }
-    })
+        square.addEventListener('mousedown', () => {
+            mouse = true;
+        })
 
-    square.addEventListener('mouseup', () => {
-        mouse = false;
-    })
+        square.addEventListener('mousemove', () => {
+            if (random && mouse) {
+                randomColour();
+                square.style.backgroundColor = colour;
+            }
+            else if (mouse) {
+                square.style.backgroundColor = colour;
+            }
+        })
+
+        square.addEventListener('mouseup', () => {
+            mouse = false;
+        })
+    }
 }
+// Normal
+let normal = document.querySelector(".normal");
+normal.addEventListener('click',() => {
+    colour = 'black';
+})
 
 // Easer
 let easer = document.querySelector(".easer");
 easer.addEventListener('click',() => {
-    colour = 'white'
+    colour = 'white';
+    random = false;
 })
 
-// Normal
-let normal = document.querySelector(".normal");
-normal.addEventListener('click',() => {
-    colour = 'black'
+// Rainbow
+let rainbow = document.querySelector(".rainbow");
+rainbow.addEventListener('click',() => {
+    return random = true;
 })
+
+function randomColour() {
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    return colour = `rgb(${r}, ${g}, ${b})`
+}
 
 // Reset
 let reset = document.querySelector(".reset");
-let square = document.querySelector(".square");
+let squares = document.querySelectorAll(".square");
 reset.addEventListener('click',() => {
-    square.style.backgroundColor = 'white';
+    squares.forEach((squares) => {
+            squares.style.backgroundColor = 'white';
+    })
+    random = false;
+    colour = 'black';
 })
+
+// Grid size using a slider 
+// const sliderValue = document.querySelector("#value");
+// const input = document.querySelector("#slider");
+// sliderValue.textContent = input.value;
+// input.addEventListener("input", (event) => {
+//     interactive(userInput = Number(event.target.value));
+//     sliderValue.textContent = event.target.value;
+// });
